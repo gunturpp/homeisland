a8e1904a91796ba8c406be5ea42bfd0fcf76af2c
wiranegara777 <muhammadwiranegara777@gmail.com>
2017-10-14 12:56:37 +0700

    tambah front-end
    sdajsd

--- src/pages/homestay/homestay.ts
+++ src/pages/homestay/homestay.ts
@@ -20,6 +20,30 @@ export class HomestayPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomestayPage');
-  }
+ //  	// var y = getDistanceFromLatLonInKm(-6.627006,106.822782,-6.625834,106.821408);
+	//   function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+	//   var R = 6371; // Radius of the earth in km
+	//   var dLat = deg2rad(lat2-lat1);  // deg2rad below
+	//   var dLon = deg2rad(lon2-lon1); 
+	//   var a = 
+	//     Math.sin(dLat/2) * Math.sin(dLat/2) +
+	//     Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+	//     Math.sin(dLon/2) * Math.sin(dLon/2); 
+	//   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+	//   var d = R * c; // Distance in km
+	//   document.getElementById("demo").innerHTML = "jaraknya:"+Math.round(d*1000)+"KM";
+	//  // return d;
+	    
+	// //  document.getElementById("demo").innerHTML = d+" km";
+	// }
+	// //document.getElementById("demo").innerHTML = "jaraknya:"+Math.round(y*1000)+"KM";
+	// function deg2rad(deg) {
+ //  	return deg * (Math.PI/180)
+	// }
 
+	// 	getDistanceFromLatLonInKm(-6.627006,106.822782,-6.625834,106.821408);
+	// 	//document.getElementById("demo").innerHTML = "jaraknya:"+Math.round(y*1000)+"KM";
+  }
+  
+	
 }
