c6157f77bf17cda064eb58e047401673510b6787
wiranegara777 <muhammadwiranegara777@gmail.com>
2017-11-10 23:30:10 +0700

    Aplikasi fungsi Log In

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams,ToastController,LoadingController  } from 'ionic-angular';
 import { HomePage } from '../home/home';
 import { TabsPage } from '../tabs/tabs';
 import { SignupPage } from '../signup/signup';
+import { UserDataProvider } from '../../provider/user-data';
+import { Http } from '@angular/http';
+import { NgForm } from '@angular/forms';
+
 /**
  * Generated class for the LoginPage page.
  *
@@ -17,10 +21,70 @@ import { SignupPage } from '../signup/signup';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  user: {username?: string, password?: string} = {};
+  submitted = false;
+  constructor(public navCtrl: NavController,
+             public navParams: NavParams,
+             public http: Http,
+             public toastCtrl: ToastController,
+             public userDataProvider: UserDataProvider,
+             public loadCtrl: LoadingController) {
   		this.navCtrl = navCtrl;
   }
 
+  onLogin(form: NgForm) {
+    this.submitted = true;
+    let loading = this.loadCtrl.create({
+        content: 'Tunggu sebentar...'
+    });
+
+    if (form.valid) {
+    loading.present();
+      let input = JSON.stringify({
+        username: this.user.username,
+        password: this.user.password
+      });
+        this.http.post("http://127.0.0.1/homeisland/backend/login.php",input).subscribe(data => {
+           let response = data.json();
+           loading.dismiss();
+           if(response.status == 200) {
+             let user=response.data;
+             this.userDataProvider.login(user.user_id,user.username);
+             console.log(user);
+
+             this.navCtrl.setRoot(TabsPage,{},{animate:true, direction:'forward'});
+             this.showAlert(response.message);
+            // if(response.data.user_status =="customer"){
+              //  this.navCtrl.push(TabsCustomer);
+            // }
+            // else{
+            //   this.navCtrl.push(TabsPage);
+             //}
+
+
+           } else {
+             this.showAlert(response.message);
+           }
+        }, err => {
+           loading.dismiss();
+           this.showError(err);
+        });
+
+    }
+  }
+
+  showError(err: any){
+    err.status==0?
+    this.showAlert("Tidak ada koneksi. Cek kembali sambungan Internet perangkat Anda"):
+    this.showAlert("Tidak dapat menyambungkan ke server. Mohon muat kembali halaman ini");
+  }
+  showAlert(val){
+    let toast = this.toastCtrl.create({
+      message: val,
+      duration: 3000
+    });
+    toast.present();
+  };
 
   geser(){
   	this.navCtrl.setRoot(TabsPage,{},{animate:true, direction:'forward'});
