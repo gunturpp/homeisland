821f127efdca4714efef788272fcdbf2973597c3
wiranegara777 <muhammadwiranegara777@gmail.com>
2017-12-12 00:21:46 +0700

    update php for rating

--- homeislandapp/backend/getAVGrating.php
+++ homeislandapp/backend/getAVGrating.php
@@ -9,13 +9,21 @@
     $id = $_GET['id_homestay'];
     $query_user = mysqli_query($conn, "SELECT round(AVG(rating),1) as rata from rating WHERE id_homestay = $id");
 
+    $query_user2 = mysqli_query($conn, "SELECT round(AVG(rating),0) as rata from rating WHERE id_homestay = $id");
+
     $result_set = array();
     while($result =mysqli_fetch_assoc($query_user)){
         $result_set[]=$result;
     }
+
+     $result_set2 = array();
+    while($result2 =mysqli_fetch_assoc($query_user2)){
+        $result_set2[]=$result2;
+    }
 $data =array(
     'message' => "Get Data User Succses",
     'data' => $result_set,
+    'data2' => $result_set2,
     'status' => "200"
 );
 
--- homeislandapp/backend/getDetailHomestay.php
+++ homeislandapp/backend/getDetailHomestay.php
@@ -9,6 +9,13 @@
     $id = $_GET['id'];
     $query_user = mysqli_query($conn, "SELECT * FROM homestay where id_homestay = $id");
 
+    $query_user2 = mysqli_query($conn, "SELECT COUNT(rating) as sum FROM rating where id_homestay = $id");
+
+    $result_set2 = array();
+    while($result2 =mysqli_fetch_assoc($query_user2)){
+        $result_set2[]=$result2;
+    }
+
     $result_set = array();
     while($result =mysqli_fetch_assoc($query_user)){
         $result_set[]=$result;
@@ -16,6 +23,7 @@
 $data =array(
     'message' => "Get Data User Succses",
     'data' => $result_set,
+    'data2' => $result_set2,
     'status' => "200"
 );
 
