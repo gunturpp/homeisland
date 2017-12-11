4db3cb68d9019da3b92055d5fecdb52958a8e634
RFTaurus <rakishfrisky@gmail.com>
2017-12-11 13:30:27 -0800

    layout

--- homeislandadmin/resources/views/layout.blade.php
+++ homeislandadmin/resources/views/layout.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Homeisland Userlist hah Application</title>
+	<title>Homeisland Application</title>
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
 </head>
 <body>
