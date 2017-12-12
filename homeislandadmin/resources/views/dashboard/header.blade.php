04d4fffe6e7785074d1d135707836eab4788cb70
RFTaurus <rakishfrisky@gmail.com>
2017-12-12 10:24:11 -0800

    nothing special

--- homeislandadmin/resources/views/dashboard/header.blade.php
+++ homeislandadmin/resources/views/dashboard/header.blade.php
@@ -36,6 +36,7 @@
 <!-- Google Font -->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
+
 <body class="hold-transition skin-blue sidebar-mini">
 <div class="wrapper">
 
@@ -60,30 +61,30 @@
         <!-- Authentication Links -->
         
         @guest
-                            {{--  <li><a href="{{ route('cruds') }}">CRUD</a></li>  --}}
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+          {{--  <li><a href="{{ route('cruds') }}">CRUD</a></li>  --}}
+            <li><a href="{{ route('login') }}">Login</a></li>
+              <li><a href="{{ route('register') }}">Register</a></li>
+                @else
+                  <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+                      {{ Auth::user()->name }} <span class="caret"></span>
+                    </a>
 
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
+                    <ul class="dropdown-menu">
+                      <li>
+                        <a href="{{ route('logout') }}"
+                          onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                          Logout
+                        </a>
 
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                          {{ csrf_field() }}
+                        </form>
+                      </li>
+                    </ul>
+                  </li>
+                @endguest
       </ul>
     </div>
   </nav>
--- homeislandadmin/resources/views/souvenir/show.blade.php
+++ homeislandadmin/resources/views/souvenir/show.blade.php
@@ -1,3 +1,5 @@
+@include('layouts.app')
+
 @extends('layout')
 
 @section('content')
