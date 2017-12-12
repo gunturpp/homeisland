a33675fb855e6c6749d17c4c86ad8f25b7ccca0c
wiranegara777 <muhammadwiranegara777@gmail.com>
2017-12-13 04:41:17 +0700

    backend php

--- homeislandapp/backend/iseng.php
+++ homeislandapp/backend/iseng.php
@@ -1,7 +1,26 @@
 <?php
 
-$number='9000000';
-echo number_format($number, 0, '', '.');
+//$number='9000000';
+//echo number_format($number, 0, '', '.');
+
+
+//$stringsCollection = [];
+
+// while (sizeof($stringsCollection) != 6){
+// 	$randString = random_string($length);
+
+// 	if (!isset($stringsCollection[$randString])){
+// 		$stringsCollection[$randString] = 1;
+// 	}
+
+// }
+// print_r(array_keys($stringsCollection));
+
+$chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+$arr = str_split($chars,1);
+shuffle($arr);
+echo substr(implode('', $arr), 0,10);
 ?>
 
 
