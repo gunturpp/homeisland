49170a079084541991869c4193ebb2c7a6e62ae8
wiranegara777 <muhammadwiranegara777@gmail.com>
2017-12-12 09:18:20 +0700

    update backend PHP

--- homeislandapp/backend/CekRating.php
+++ homeislandapp/backend/CekRating.php
@@ -7,7 +7,8 @@
   include 'config.php';
     //$id=$_GET['user'];
     $id = $_GET['id'];
-    $query_user = mysqli_query($conn, "SELECT * FROM rating where id_user = $id");
+    $id_homestay = $_GET['id_homestay'];
+    $query_user = mysqli_query($conn, "SELECT * FROM rating where id_user = $id AND id_homestay = $id_homestay");
 
     if (mysqli_num_rows($query_user)){
 
